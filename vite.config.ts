e5c4e2b53dff1106e87dfb6bb2fc997e949f8182
crediedge074-ux <dweglarz074@gmail.com>
2026-07-09 18:13:39 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,8 @@ export default defineConfig({
     server: { entry: "server" },
   },
   nitro: {
-    preset: "vercel",
+    preset: "cloudflare-module",
+    output: { dir: "dist", serverDir: "dist/server", publicDir: "dist/client" },
+    cloudflare: { nodeCompat: true, deployConfig: true },
   },
 });
